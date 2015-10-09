c99d69496f545960b656a8df6ba68c3415ec0355
brigmac <brigmac@umich.edu>
2015-10-09 12:33:30 -0400

    Update JSONget.java

--- JSONget.java
+++ JSONget.java
@@ -1,44 +1,101 @@
-//This class is for testing by changing the inputs of the Taskdoer.
+//This class parses the JSON file, creates java objects (tasks), and then puts the
+//objects into a list. The list is the list of tasks.
+
+//Needs a specific filepath to task-3.JSON
 
 package brigid_BP3CodingChallenge;
 
-
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 
-public class TaskTester
+
+public class JSONget
 {
-
-	public static void main(String[] args) throws java.text.ParseException 
+	public static List<Task> readJSONTaskList() 
 	{
-		List<Task> listOfTasks = JSONget.readJSONTaskList();
-		if(listOfTasks == null)
+		JSONParser parser = new JSONParser();
+		try 
 		{
-			System.out.println("List error occured");
-			System.exit(1);
+			List<Task> myTaskList = new LinkedList<Task>();
+			
+			// needs a specific file path
+			JSONArray arr = (JSONArray) parser.parse(new FileReader("/Users/brigmac/Downloads/task-3.json"));
+			
+			for (Object o: arr) 
+			{
+				// the () means casting
+				JSONObject task = (JSONObject) o;
+				
+				//gets the instanceId
+				long instanceId = (long) task.get("instanceId");
+				
+				//gets the assignee
+				String theAssignee = (String) task.get("assignee");
+				
+				//gets the task status
+				String taskStatus = (String) task.get("status");
+				
+				//gets the createDate
+				String theCreateDate = (String) task.get("createDate");
+				DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+				Date crtDate = format.parse(theCreateDate);
+		
+				//gets the closeDate
+				String theEndDate = (String) task.get("closeDate");
+				Date clsdDate; 
+				
+				//accounts for nulls aka they haven't been closed yet
+				if (theEndDate != null) 
+				{
+					DateFormat format1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+					clsdDate = format1.parse(theEndDate);
+				}
+				else 
+				{
+					clsdDate = null; 
+				}
+				
+				//gets the task id
+				String theTaskId = (String) task.get("id");
+				
+				//get the task name
+				String theTaskName = (String) task.get("name");
+				
+				//adds elements to the task
+				Task t = new Task(instanceId, theAssignee, taskStatus, crtDate, clsdDate, theTaskId, theTaskName);
+				
+				//adds the task to the list
+				myTaskList.add(t);
+	
+		  }
+			return myTaskList;
 		}
-		
-		Taskdoer td = new Taskdoer();
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-		String dateString = "2014-11-20T00:32:27";
-		Date dateObject = sdf.parse(dateString); // Handle the ParseException here
-		td.openCloseTask(listOfTasks, dateObject);
-		
-		String startDateString = "2014-8-20T00:32:27";
-		String endDateString = "2014-11-20T00:32:27";
-		Date startDate = sdf.parse(startDateString);
-		Date endDate = sdf.parse(endDateString);
-		td.openCloseTaskRange(listOfTasks, startDate, endDate);
-		
-		td.getMostRecentTask(listOfTasks, 477);
-		
-		td.getCountOfTask(listOfTasks, 477);
-		
-		td.assigneeOpenCloseTasks(listOfTasks, "Impact 2014");
-		
+
+		catch (ParseException p) 
+		{
+			System.out.println("Failed during parse");
+	  }
+		catch (IOException fileNotFound) 
+		{
+			System.out.println("Could not find file");
+	  }
+		catch (java.text.ParseException e)
+		{
+			System.out.println("Date failed");
+		}
+	return null;
 	}
+
 }
+	
